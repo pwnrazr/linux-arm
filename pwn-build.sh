08e30853202bfc10ae32d499dc22a670a75ce46d
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2021-10-10 13:11:30 +0800

    build: add build script

---
+++ pwn-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+OUT_DIR=out/
+
+export KBUILD_BUILD_HOST=PWN-PC
+
+START=$(date +"%s")
+
+make ARCH=arm64 \
+     CROSS_COMPILE=aarch64-linux-gnu- \
+     O=${OUT_DIR} \
+     armbian_defconfig \
+     -j$(nproc)
+
+make ARCH=arm64 \
+     CROSS_COMPILE=aarch64-linux-gnu- \
+     O=${OUT_DIR} \
+     Image modules dtbs bindeb-pkg \
+     -j$(nproc)
+
+END=$(date +"%s")
+DIFF=$(( END - START))
+echo -e '\033[01;32m' "Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds"
