1ddfc813bd4c35722e56d04475652d64c029aa32
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2021-10-13 20:59:41 +0800

    scripts: menuconfig: add cross_compile flag

--- pwn-menuconfig.sh
+++ pwn-menuconfig.sh
@@ -1,5 +1,5 @@
-make ARCH=arm64 armbian_defconfig
-make ARCH=arm64 menuconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- armbian_defconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- menuconfig
 cp .config arch/arm64/configs/armbian_defconfig
 make mrproper
 
