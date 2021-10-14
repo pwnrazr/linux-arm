b29d1b38b61e8a2cb3bac770e454e8ce19ddb3a3
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2021-10-15 07:51:34 +0800

    scripts: pwn-menuconfig: use llvm

--- pwn-menuconfig.sh
+++ pwn-menuconfig.sh
@@ -4,8 +4,29 @@ HOME=/home/pwnrazr
 # Set compiler PATH
 PATH=${HOME}/proton-clang/bin/:$PATH
 
-make ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- armbian_defconfig
-make ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- menuconfig
+make ARCH=arm64 \
+	CC=clang \
+	LD="ld.lld" \
+	AR="llvm-ar" \
+	NM="llvm-nm" \
+	OBJCOPY="llvm-objcopy" \
+	OBJDUMP="llvm-objdump" \
+	STRIP="llvm-strip" \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	armbian_defconfig
+
+make ARCH=arm64 \
+	CC=clang \
+	LD="ld.lld" \
+	AR="llvm-ar" \
+	NM="llvm-nm" \
+	OBJCOPY="llvm-objcopy" \
+	OBJDUMP="llvm-objdump" \
+	STRIP="llvm-strip" \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	menuconfig
+
 cp .config arch/arm64/configs/armbian_defconfig
+
 make mrproper
 
