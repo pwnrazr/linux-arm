99c03c08f83ef8c8d40fde6e6cea3c472f3e0675
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2021-10-15 07:05:21 +0800

    scripts: pwn-menuconfig: use clang

--- pwn-menuconfig.sh
+++ pwn-menuconfig.sh
@@ -1,5 +1,11 @@
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- armbian_defconfig
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- menuconfig
+# HOME path
+HOME=/home/pwnrazr
+
+# Set compiler PATH
+PATH=${HOME}/proton-clang/bin/:$PATH
+
+make ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- armbian_defconfig
+make ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- menuconfig
 cp .config arch/arm64/configs/armbian_defconfig
 make mrproper
 
